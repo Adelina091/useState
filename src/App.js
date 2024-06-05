a75cb85a6886cc806774797ecd96706a40cbd3fe
Adelina091 <adelina.kabirova19@gmail.com>
2024-06-05 20:00:30 +0500

    aads

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { MyComponent } from "./components/test.useState"; // Убедитесь, что путь к компоненту указан правильно
+import { MyComponent } from "./components/test.useState"; 
 
 function App() {
   return (
